aa89b5774cc8efaab33243cbdae430b435ca9e9f
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-06-22 21:09:49 +0000

    facebook authentication and callback done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show], controller: 'users/users'
 end
