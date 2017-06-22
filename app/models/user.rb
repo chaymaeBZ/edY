37d3541405168888e778ae2ef33387c80a48d964
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-06-22 15:28:15 +0000

    facebook callback schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+  
+  def self.from_omniauth
+     
+  end
 end
+
