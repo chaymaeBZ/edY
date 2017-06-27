04eb5855fe65d2adef0aca6eeee378e2cd980cce
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-06-27 11:22:28 +0000

    working on admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627090239) do
+ActiveRecord::Schema.define(version: 20170627110843) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20170627090239) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
