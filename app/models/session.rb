24992a941777b9c0253ad35a2c2bb4dd1a2b0351
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-06-27 09:06:56 +0000

    sessions users associations

--- app/models/session.rb
+++ app/models/session.rb
@@ -4,5 +4,6 @@ class Session < ApplicationRecord
   validates :title, presence: true, length: { maximum: 150 }
   validates :description, presence: true, length: { minimum: 50 }
   has_and_belongs_to_many :courses
+  has_and_belongs_to_many :users
   scope :active, -> { where("? BETWEEN start_date and end_date", Time.now.to_date)}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626220821) do
+ActiveRecord::Schema.define(version: 20170627090239) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 20170626220821) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.decimal  "price"
+  end
+
+  create_table "sessions_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "session_id"
+    t.index ["session_id"], name: "index_sessions_users_on_session_id"
+    t.index ["user_id"], name: "index_sessions_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
